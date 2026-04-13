6e941290f52c5f8ef83dad09be279b3cc316b294
mican8saku <max.ihren@gmail.com>
2026-04-13 17:44:57 +0200

    Update dhtsensor-test.py
    Uppdaterat kommentar om vilket pinben det är kopplad till

--- dhtsensor-test.py
+++ dhtsensor-test.py
@@ -3,7 +3,7 @@ import board
 import adafruit_dht
 from flask import Flask, render_template_string
 
-# Initiera DHT11 på GPIO 4 (Pin 7)
+# Initiera DHT11 på GPIO 26
 dht_device = adafruit_dht.DHT11(board.D26)
 
 app = Flask(__name__)
