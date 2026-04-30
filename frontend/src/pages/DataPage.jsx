eef9a8e138135751e927c5198bcbdae4bb910418
Raspberry Pi - Project Group 3 <group3@canary.local>
2026-04-30 18:55:45 +0200

    ta bort micro bug

--- frontend/src/pages/DataPage.jsx
+++ frontend/src/pages/DataPage.jsx
@@ -1,4 +1,4 @@
-<65;53;14M<65;53;14M<65;53;14M<65;53;14Mimport React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { 
   LineChart, Line, AreaChart, Area, XAxis, YAxis, 
   CartesianGrid, Tooltip, ResponsiveContainer 
