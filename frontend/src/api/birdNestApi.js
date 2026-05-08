52b006eefee253c9b8b20226020a99d31c5d691b
mihren <mihren@kth.se>
2026-05-08 19:35:57 +0200

    updated VITE URL

--- frontend/src/api/birdNestApi.js
+++ frontend/src/api/birdNestApi.js
@@ -3,7 +3,8 @@
  */
 
 // export const BASE_URL = import.meta.env.VITE_API_URL || "http://100.81.230.81:5000";
-export const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+export const BASE_URL = import.meta.env.VITE_API_URL;
+if (!BASE_URL) console.warn("VITE_API_URL is not set!");
 
 async function parseJsonSafely(res) {
   try {
