b40d85b8d211e47fc4b117db30c398e8a9610124
mihren <mihren@kth.se>
2026-05-08 20:44:56 +0200

    fick inte rotera

--- main.py
+++ main.py
@@ -412,9 +412,8 @@ def camera_producer_thread():
         cmd = [
             'rpicam-vid', '-t', '0', 
             '--inline', 
-            '--width', '1280', '--height', '960', # Bas-upplösning (liggande)
-            '--rotation', '90',                   # Roterar den till 960x1280 (stående)
-            '--vflip', '1', '--hflip', '1',       # Behåll dessa om bilden är spegelvänd
+            '--width', '960', '--height', '1280', # Direkt stående
+            '--vflip', '1', '--hflip', '1', 
             '--framerate', '20', 
             '--codec', 'mjpeg',
             '-n', '-o', '-'
