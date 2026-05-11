3169fa79fbf0ac331b6c4b92e91586083b10fbb7
mihren <mihren@kth.se>
2026-05-11 15:18:18 +0200

    starting led pin as low

--- main.py
+++ main.py
@@ -143,6 +143,7 @@ try:
     
     GPIO.setup(PIR_PIN, GPIO.IN)
     GPIO.setup(LED_PIN, GPIO.OUT)
+    GPIO.output(LED_PIN, GPIO.LOW)
     GPIO.setup(BUTTON_UP, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(BUTTON_DOWN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(LEDSTRIP_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
