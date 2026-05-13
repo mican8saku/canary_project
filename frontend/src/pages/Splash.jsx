3371ee2c006632a83d69dbbd7a7a940e504c24d6
mihren <mihren@kth.se>
2026-05-14 01:31:14 +0200

    provar med 4000 ms

--- frontend/src/pages/Splash.jsx
+++ frontend/src/pages/Splash.jsx
@@ -6,7 +6,7 @@ export default function Splash() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    const timer = setTimeout(() => navigate("/home"), 2500);
+    const timer = setTimeout(() => navigate("/home"), 4000);
     return () => clearTimeout(timer);
   }, [navigate]);
 
