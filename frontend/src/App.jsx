390984b969c3820505256169f50a3c08129d2465
mihren <mihren@kth.se>
2026-05-14 01:25:31 +0200

    ändrat så splash blir spinner

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,11 +18,12 @@ const AuthenticatedApp = () => {
 
   // Show loading spinner while checking app public settings or auth
   if (isLoadingPublicSettings || isLoadingAuth) {
-    return (
-      <div className="fixed inset-0 flex items-center justify-center">
-        <div className="w-8 h-8 border-4 border-slate-200 border-t-slate-800 rounded-full animate-spin"></div>
-      </div>
-    );
+    return <Splash />;
+    // return (
+    //   <div className="fixed inset-0 flex items-center justify-center">
+    //     <div className="w-8 h-8 border-4 border-slate-200 border-t-slate-800 rounded-full animate-spin"></div>
+    //   </div>
+    // );
   }
 
   // Handle authentication errors
